bd2f74b2367d5987108ef138c511c810b1f250da
Cioclea Doru <doru.cioclea@corebuild.eu>
2020-07-19 17:20:32 +0300

    Initial version of enterpriseapp generated by JHipster-6.4.1

---
+++ microservices/main/src/test/java/com/enterpriseapp/main/cucumber/stepdefs/StepDefs.java
@@ -0,0 +1,9 @@
+package com.enterpriseapp.main.cucumber.stepdefs;
+
+import org.springframework.test.web.servlet.ResultActions;
+
+public abstract class StepDefs {
+
+    protected ResultActions actions;
+
+}
